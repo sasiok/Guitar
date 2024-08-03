0f0ef57dd85224fc5a42d5de269bc1628dd88af8
sasiok <sashokivanovyck@gmail.com>
2024-08-03 21:00:33 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict"
+
+const swiper = new Swiper('.swiper-reviews', {
+    // Optional parameters
+    loop: true,
+    autoHeight: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+});
